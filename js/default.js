c42bd67f6729ac39b40cfcf79fd80e788d87a36a
ishimaomao <38420784+ishimaomao@users.noreply.github.com>
2018-11-19 18:12:05 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -14,3 +14,13 @@ While(0 < HP>{
   document.getElementById('while').appendChild(li);
 }
   
+var include_tax = function(price){
+  return Math.floor(price + 0.08 * price);
+}
+  
+for(var i = 1; i < 10; i++){
+  var li = document.createElement('li');
+  var price = Math.floor(Math.random() * 1000);
+  li.textContent = price + '円(税込み' + include_tax(price) + '円)';
+  document.getElementById('price').appendChild(li);
+}
